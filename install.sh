0bc2996c5d972bcb103100220a5bfaf79f0dd06d
Skyler Spaeth <skylerspaeth.scc@me.com>
2022-08-03 23:19:54 -0500

    run all default modules dynamically

--- install.sh
+++ install.sh
@@ -13,5 +13,6 @@ export -f gclonecd
 # ensure flathub is enabled for modules to get software from
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-modules/default/theme
-modules/default/wallpapers
+for MODULE in $(find default/ -type f); do
+  bash -e "$MODULE" || echo "$MODULE failed."; break
+done
